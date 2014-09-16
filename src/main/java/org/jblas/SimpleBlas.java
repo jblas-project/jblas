ad2c8a8aa5e6f47cc554cd2803de1094bfc74d6c
Feiteng Li <lifeiteng0422@gmail.com>
2014-09-16 15:31:17 +0800

    gemv BUG
    public static FloatMatrix gemv(float alpha, FloatMatrix a, FloatMatrix x, float beta, FloatMatrix y)  or DoubleMatrix 's implementation has two errors:
    1. when beta!=0, original implementation will have java.lang.ArrayIndexOutOfBoundsException: " j < a.columns;"
    2. forget multiple alpha when do "y.data[i] += a.get(i, j) * xj;"

--- src/main/java/org/jblas/SimpleBlas.java
+++ src/main/java/org/jblas/SimpleBlas.java
@@ -195,13 +195,23 @@ public class SimpleBlas {
 							y.data[i] += a.get(i, j) * xj;
 					}
 				}
-			} else {
-				for (int j = 0; j < a.columns; j++) {
-					double byj = beta * y.data[j];
-					double xj = x.get(j);
-					for (int i = 0; i < a.rows; i++)
-						y.data[j] = a.get(i, j) * xj + byj;
-				}
+			} else {// mayebe we can rm if else?
+				for (int i = 0; i < y.length; i++)
+		                    y.data[i] = beta * y.data[i];
+	
+		                for (int j = 0; j < a.columns; j++) {
+		                    double xj = x.get(j);
+		                    if (xj != 0.0) {
+		                        for (int i = 0; i < a.rows; i++)
+		                            y.data[i] += alpha * a.get(i, j) * xj;
+		                    }
+		                }
+//				for (int j = 0; j < a.columns; j++) {
+//					double byj = beta * y.data[j];
+//					double xj = x.get(j);
+//					for (int i = 0; i < a.rows; i++)
+//						y.data[j] = a.get(i, j) * xj + byj;
+//				}
 			}
 		}
 		return y;
@@ -621,13 +631,24 @@ public class SimpleBlas {
 							y.data[i] += a.get(i, j) * xj;
 					}
 				}
-			} else {
-				for (int j = 0; j < a.columns; j++) {
-					float byj = beta * y.data[j];
-					float xj = x.get(j);
-					for (int i = 0; i < a.rows; i++)
-						y.data[j] = a.get(i, j) * xj + byj;
-				}
+			} else {// mayebe we can rm if else? 
+				for (int i = 0; i < y.length; i++)
+		                    y.data[i] = beta * y.data[i];
+	
+		                for (int j = 0; j < a.columns; j++) {
+		                    double xj = x.get(j);
+		                    if (xj != 0.0) {
+		                        for (int i = 0; i < a.rows; i++)
+		                            y.data[i] += alpha * a.get(i, j) * xj;
+		                    }
+		                }
+//				for (int j = 0; j < a.columns; j++) {
+//					double byj = beta * y.data[j];
+//					double xj = x.get(j);
+//					for (int i = 0; i < a.rows; i++)
+//						y.data[j] = a.get(i, j) * xj + byj;
+//				}			
+
 			}
 		}
 		return y;
